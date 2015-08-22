fb5ffa63ce5a77c7fe2f668ad1b4dc05461afe66
Dennis Bang <jhbang8917@gmail.com>
2015-08-22 13:05:47 +0900

    add data-structures

---
+++ data-structures/java/src/LinkedList/TestLinkedList.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+/**
+ * Created by jhbang on 2015. 5. 27..
+ */
+public class TestLinkedList {
+
+
+    public static void main(String[] args){
+        LinkedList<String> list = new LinkedList();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+        System.out.println(list.toString());
+
+    }
+
+}
---
+++ data-structures/java/src/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jhbang on 2015. 5. 27..
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
