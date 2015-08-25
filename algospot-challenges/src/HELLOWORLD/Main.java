aeac2cac3f1768d55af576eacc8ea915a051847d
Dennis Bang <jhbang8917@gmail.com>
2015-08-25 12:20:47 +0900

    add algospot-challenges

---
+++ algospot-challenges/src/HELLOWORLD/Main.java
@@ -0,0 +1,10 @@
+package HELLOWORLD;
+
+/**
+ * Created by jhbang on 2015. 8. 25..
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
